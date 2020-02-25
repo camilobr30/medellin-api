ff4fd1f6e48e3a84c8efcc43653caa63bbbcd9a0
Camilo Barbosa <camilobr30@gmail.com>
2020-02-25 18:26:45 -0500

    Añadir Validator celebrate

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
 const router = express.Router();
+const { celebrate, Joi, Segments } = require('celebrate');
 const { UsersController } = require('../controllers');
 
-router.post('/users', UsersController.createUser);
+router.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      first_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      email: Joi.string().required(),
+    }),
+  }),
+  UsersController.createUser,
+);
 router.get('/users', UsersController.getUsers);
 router.get('/users/:id', UsersController.getUserById);
 router.patch('/users/:id', UsersController.updateUserById);
